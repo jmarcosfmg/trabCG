ed5f49b4c1af3967c7e8376273b0102c825910a0
João <jmarcosfmg@hotmail.com>
2019-05-14 02:57:07 -0300

    pronto

--- trab1.py
+++ trab1.py
@@ -2,9 +2,8 @@ import sys,pygame
 from pygame import gfxdraw
 
 pygame.init()
-screen = pygame.display.set_mode((400,400))
-tela = pygame.Surface((500, 400))
-
+screen = pygame.display.set_mode((600,400))
+tela = pygame.Surface((600, 400))
 white=(255,255,255)
 black=(0,0,0)
 red = (255,0,0)
@@ -21,10 +20,21 @@ green = (0,128,0)
 purple = (128,0,128)
 teal = (0,128,128)
 navy = (0,0,128)
-screen.fill(white)
+
+menu_font = pygame.font.Font(None, 17)
+
+limpar_txt = menu_font.render("Limpar", True, black)
+linha_txt = menu_font.render("Linha", True, black)
+retangulo_txt = menu_font.render("Retangulo", True, black)
+quadrado_txt = menu_font.render("Quadrado", True, black)
+circulo_txt = menu_font.render("Circulo", True, black)
+polilinha_txt = menu_font.render("Polilinha", True, black)
+curva_txt = menu_font.render("Curva", True, black)
+preenchimento_txt = menu_font.render("Preench.", True, black)
 lista_cores = [white,silver,gray,black,maroon,red,magenta,purple,navy,blue,teal,teal,aqua,lime,green,olive,yellow]
 color = black #default
-
+funcao = 1 #1 = Bresenham #2=...
+preenchido = False
 menu_rect = pygame.Rect(0, 0, 100, 400)
 screen_rect = pygame.Rect(100, 0, 400, 400)
 
@@ -44,12 +54,18 @@ lime_rect = pygame.Rect(5, 157, 20, 20)
 green_rect = pygame.Rect(27, 157, 20, 20)
 olive_rect = pygame.Rect(5, 179, 20, 20)
 yellow_rect = pygame.Rect(27, 179, 20, 20)
+limpar_rect = pygame.Rect(5,200,42,20)
+linha_rect = pygame.Rect(5,222,42,20)
+retangulo_rect = pygame.Rect(5,244,42,20)
+quadrado_rect = pygame.Rect(5,266,42,20)
+polilinha_rect = pygame.Rect(5,288,42,20)
+curva_rect = pygame.Rect(5,310,42,20)
+circulo_rect = pygame.Rect(5,332,42,20)
+preenchimento_rect = pygame.Rect(5,354,42,20)
 
 lista_rect_cores = [white_rect,silver_rect,gray_rect,black_rect,maroon_rect,red_rect,magenta_rect,purple_rect,navy_rect,blue_rect,teal_rect,teal_rect,aqua_rect,lime_rect,green_rect,olive_rect,yellow_rect]
 
-draw = False
-
-def rect_b_color():
+def desenha_menu():
 	for x in range(len(lista_cores)):
 		pygame.draw.rect(screen,lista_cores[x],lista_rect_cores[x])
 		if color == lista_cores[x]:
@@ -57,54 +73,238 @@ def rect_b_color():
 		else:
 			border = 1
 			pygame.draw.rect(screen, black, lista_rect_cores[x], border)
+	pygame.draw.rect(screen,silver,limpar_rect)
+	screen.blit(limpar_txt,(7,204))
+	pygame.draw.rect(screen,silver,linha_rect)
+	screen.blit(linha_txt,(7,226))
+	pygame.draw.rect(screen,silver,retangulo_rect)
+	screen.blit(retangulo_txt,(7,248))
+	pygame.draw.rect(screen,silver,quadrado_rect)
+	screen.blit(quadrado_txt,(7,270))
+	pygame.draw.rect(screen,silver,polilinha_rect)
+	screen.blit(polilinha_txt,(7,292))
+	pygame.draw.rect(screen,silver,curva_rect)
+	screen.blit(curva_txt,(7,314))
+	pygame.draw.rect(screen,silver,circulo_rect)
+	screen.blit(circulo_txt,(7,336))
+	pygame.draw.rect(screen,silver,curva_rect)
+	screen.blit(curva_txt,(7,314))
+	if preenchido == True:
+		pygame.draw.rect(screen,silver,preenchimento_rect)
+	else:
+		pygame.draw.rect(screen,gray,preenchimento_rect)
+	screen.blit(preenchimento_txt,(7,359))
 
 # collision detection for COLOR
-def escolher_cor(mouse_pos): 
+def escolhe_menu(mouse_pos): 
 	global color
+	global funcao
+	global preenchido
 	if white_rect.collidepoint(mouse_pos):
 		color = white
-	if silver_rect.collidepoint(mouse_pos):
+	elif silver_rect.collidepoint(mouse_pos):
 		color = silver
-	if gray_rect.collidepoint(mouse_pos):
+	elif gray_rect.collidepoint(mouse_pos):
 		color = gray
-	if black_rect.collidepoint(mouse_pos):
+	elif black_rect.collidepoint(mouse_pos):
 		color = black
-	if maroon_rect.collidepoint(mouse_pos):
+	elif maroon_rect.collidepoint(mouse_pos):
 		color = maroon
-	if red_rect.collidepoint(mouse_pos):
+	elif red_rect.collidepoint(mouse_pos):
 		color = red
-	if magenta_rect.collidepoint(mouse_pos):
+	elif magenta_rect.collidepoint(mouse_pos):
 		color = magenta
-	if purple_rect.collidepoint(mouse_pos):
+	elif purple_rect.collidepoint(mouse_pos):
 		color = purple
-	if navy_rect.collidepoint(mouse_pos):
+	elif navy_rect.collidepoint(mouse_pos):
 		color = navy
-	if blue_rect.collidepoint(mouse_pos):
+	elif blue_rect.collidepoint(mouse_pos):
 		color = blue
-	if teal_rect.collidepoint(mouse_pos):
+	elif teal_rect.collidepoint(mouse_pos):
 		color = teal
-	if aqua_rect.collidepoint(mouse_pos):
+	elif aqua_rect.collidepoint(mouse_pos):
 		color = aqua
-	if lime_rect.collidepoint(mouse_pos):
+	elif lime_rect.collidepoint(mouse_pos):
 		color = lime
-	if green_rect.collidepoint(mouse_pos):
+	elif green_rect.collidepoint(mouse_pos):
 		color = green
-	if olive_rect.collidepoint(mouse_pos):
+	elif olive_rect.collidepoint(mouse_pos):
 		color = olive
-	if yellow_rect.collidepoint(mouse_pos):
+	elif yellow_rect.collidepoint(mouse_pos):
 		color = yellow
+	elif limpar_rect.collidepoint(mouse_pos):
+		limpa_tela()
+	elif linha_rect.collidepoint(mouse_pos):
+		funcao = 1
+	elif quadrado_rect.collidepoint(mouse_pos):
+		funcao = 2
+	elif polilinha_rect.collidepoint(mouse_pos):
+		funcao = 3
+	elif circulo_rect.collidepoint(mouse_pos):
+		funcao = 4
+	elif curva_rect.collidepoint(mouse_pos):
+		funcao = 5
+	elif retangulo_rect.collidepoint(mouse_pos):
+		funcao = 6
+	elif preenchimento_rect.collidepoint(mouse_pos):
+		preenchido = not preenchido
+		desenha_menu()
+		pygame.display.flip()
+		
 
-rect_b_color()
-tela.blit(screen,(0,0),menu_rect)
 
-pygame.draw.line(screen, black, (95, 40), (95, 360))
-pygame.draw.line(screen, black, (100, 40), (100, 360))
+def limpa_tela():
+	screen.fill(white)
+	desenha_menu()
+	tela.blit(screen,(0,0),menu_rect)
+	pygame.draw.line(screen, black, (55, 40), (55, 360))
+	pygame.draw.line(screen, black, (59, 40), (59, 360))
+	pygame.display.flip()
+
+
+def frange(start, stop, step):#para aceitar float
+	x = start
+	while x < stop:
+		yield x
+		x += step
+
+def bezier():
+	coord = []
+	coord[0:1] = pipf()
+	coord[2:3] = pipf()
+	for t in frange(0,1,0.01):
+		x1,y1 = coord[0] #ponto onde começa da curva
+		x2,y2 = coord[1] #primeiro ponto de controle
+		x3,y3 = coord[2] #segundo ponto de controle
+		x4,y4 = coord[3] #ponto onde termina a curva
+		omt = 1-t
+		omt2 = omt*omt
+		omt3 = omt2*omt
+		t2 = t*t
+		t3 = t2*t
+		x = omt3 * x1 + ((3*omt2)*t*x2) + (3*omt*t2*x3)+t3*x4
+		y = omt3 * y1 + ((3*omt2)*t*y2) + (3*omt*t2*y3)+t3*y4
+		
+		x = int(round(x,0))#arredonda e transforma para inteiro
+		y = int(round(y,0))
+
+		screen.set_at((x,y),color)
+		pygame.display.flip()
+
+def circle(): #cx,cy = centro da circunferencia
+	(cx,cy),(aux1,aux2) = pipf()
+	raio = int(((abs(cx-aux1)**2)+(abs(cy-aux2)**2))**(1/2))
+	if preenchido:
+		cont = raio
+	else:
+		cont = 1
+
+	for linha in range(cont):
+		x = 0
+		y = raio
+		d = 1-raio
+		while x < y:
+			plotCircle(x,y,cx,cy)
+			x += 1
+			if d < 0:
+				d += 2 * x + 1
+			else:
+				y -= 1
+				d += 2*(x-y) + 1
+		raio = raio -1
+
+	pygame.display.flip()
+
+
+def plotCircle(x,y,cx,cy):
+	gfxdraw.pixel(screen,cx+x,cy+y,color)
+	gfxdraw.pixel(screen,cx-x,cy+y,color)
+	gfxdraw.pixel(screen,cx+x,cy-y,color)
+	gfxdraw.pixel(screen,cx-x,cy-y,color)
+	gfxdraw.pixel(screen,cx+y,cy+x,color)
+	gfxdraw.pixel(screen,cx-y,cy+x,color)
+	gfxdraw.pixel(screen,cx+y,cy-x,color)
+	gfxdraw.pixel(screen,cx-y,cy-x,color)
+
+def polilinha():
+	c1, c2 = bresenham(pipf()) #pega esse ultimo ponto
+
+	while funcao == 3:
+		for event in pygame.event.get():
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				a,b,c = pygame.mouse.get_pressed()
+				if(c == True):
+					break #sai do loop ao apertar o botão esquerdo
+				else:
+					x,y = pygame.mouse.get_pos()
+					if (x<59):
+						escolhe_menu((x,y))
+						if funcao != 3: #se mudar de ferramenta
+							break
+					else:
+						c1, c2 = bresenham([(c1,c2),(x,y)])
+			if event.type == pygame.QUIT: sys.exit()
+
+def pipf(): #ponto final, ponto inicial
+	i = 0
+	posic = []
+	while i<2:
+		for e in pygame.event.get():
+			if e.type == pygame.QUIT:
+				sys.exit()
+			if e.type == pygame.MOUSEBUTTONDOWN:
+				a = pygame.mouse.get_pos()
+				if (a[0]<59):
+					escolhe_menu(a)
+				else:
+					posic.append(pygame.mouse.get_pos())
+					i = i+1
+	return posic
+
+
+def retangulo():
+	[(x,y),(x2,y2)] = pipf()
+	if preenchido:
+		if x>x2:
+			(x,y),(x2,y2) = (x2,y2),(x,y)
+		while(x<=x2):
+			bresenham([(x,y),(x,y2)])
+			x = x+1
+	else:
+		bresenham([(x,y),(x2,y)])
+		bresenham([(x,y),(x,y2)])
+		bresenham([(x,y2),(x2,y2)])
+		bresenham([(x2,y),(x2,y2)])
+
+def linha():
+	bresenham(pipf())
+
+def quadrado():
+	[(x,y),(x2,y2)] = pipf()
+	if (abs(x-x2)>abs(y-y2)):
+		lado = abs(x-x2)
+	else:
+		lado = abs(y-y2)
+	if preenchido:
+		if(x>x2):
+			(x,y),(x2,y2) = (x2,y2),(x,y)
+		x,y = x-lado,y-lado
+		x2,y2 = x+lado,y+lado
+		while(x<=x2):
+			bresenham([(x,y),(x,y2)])
+			x = x+1
+	else:
+		bresenham([(x-lado,y-lado),(x-lado,y+lado)]) #Ponto1 ao Ponto2
+		bresenham([(x-lado,y-lado),(x+lado,y-lado)]) #Ponto1 ao Ponto3
+		bresenham([(x+lado,y-lado),(x+lado,y+lado)]) #Ponto3 ao Ponto4
+		bresenham([(x-lado,y+lado),(x+lado,y+lado)]) #Ponto2 ao Ponto4
 
 def bresenham(coord):
 	start = coord[0]
 	end = coord[1]
 	x1, y1 = start
 	x2, y2 = end
+
 	tam_vert = pygame.display.get_surface().get_height()
 	
 	if x1>x2:
@@ -151,27 +351,20 @@ def bresenham(coord):
 	pygame.display.flip()
 	return end #retorna o ultimo ponto
 
-def clica():
-	i = 0
-	posic = []
-	while i<2:
-		for e in pygame.event.get():
-			if e.type == pygame.QUIT:
-				sys.exit()
-			if e.type == pygame.MOUSEBUTTONDOWN:
-				a = pygame.mouse.get_pos()
-				if (a[0]<100):
-					escolher_cor(a)
-				else:
-					posic.append(pygame.mouse.get_pos())
-					i = i+1
-	return posic
-
-
-pygame.display.flip()
-while 1:
-	
-	bresenham(clica())
+limpa_tela()
+while True:
+	if funcao == 1:
+		linha()
+	elif funcao == 2:
+		quadrado()
+	elif funcao == 3:
+		polilinha()
+	elif funcao == 4:
+		circle()
+	elif funcao == 5:
+		bezier()
+	elif funcao == 6:
+		retangulo()
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT: sys.exit()
